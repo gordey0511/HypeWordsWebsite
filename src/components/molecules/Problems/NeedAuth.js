5bc64fc92576db3882eef68582d3a3ce6d63f232
i3cheese <i3cheese@yandex.ru>
2022-02-11 16:41:15 +0700

    enable auth

--- src/components/molecules/Problems/NeedAuth.js
+++ src/components/molecules/Problems/NeedAuth.js
@@ -15,10 +15,10 @@ export const checkUserAuth = (user_id, verified_email) => {
     user_id !== undefined &&
     user_id !== null &&
     user_id !== ''
-    //   &&
-    // verified_email !== undefined &&
-    // verified_email !== null &&
-    // verified_email === true
+      &&
+    verified_email !== undefined &&
+    verified_email !== null &&
+    verified_email === true
   ) {
     return true
   }
